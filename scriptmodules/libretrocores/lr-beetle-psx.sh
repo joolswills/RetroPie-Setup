68056e1fb45edd08f0a824946c623573269cacbc
dankcushions <dankcushions@users.noreply.github.com>
2020-06-03 14:43:11 +0100

    PSX: Beetle as experimental core for aarch64, with dynarec enabled.

--- scriptmodules/libretrocores/lr-beetle-psx.sh
+++ scriptmodules/libretrocores/lr-beetle-psx.sh
@@ -13,11 +13,12 @@ rp_module_id="lr-beetle-psx"
 rp_module_desc="PlayStation emulator - Mednafen PSX Port for libretro"
 rp_module_help="ROM Extensions: .bin .cue .cbn .img .iso .m3u .mdf .pbp .toc .z .znx\n\nCopy your PlayStation roms to $romdir/psx\n\nCopy the required BIOS files\n\nscph5500.bin and\nscph5501.bin and\nscph5502.bin to\n\n$biosdir"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-psx-libretro/master/COPYING"
-rp_module_section="opt x86=main"
+rp_module_section="opt x86=main aarch64=exp"
 rp_module_flags="!arm"
 
 function depends_lr-beetle-psx() {
-    local depends=(libvulkan-dev libgl1-mesa-dev)
+    local depends=()
+    isPlatform "x86" && depends+=(libvulkan-dev libgl1-mesa-dev)
     getDepends "${depends[@]}"
 }
 
@@ -26,23 +27,37 @@ function sources_lr-beetle-psx() {
 }
 
 function build_lr-beetle-psx() {
+    local params=()
+    local target="mednafen_psx_libretro.so"
+    if isPlatform "x86"; then
+        params+=(HAVE_HW=1)
+        target="mednafen_psx_hw_libretro.so"
+    fi
     make clean
-    make HAVE_HW=1
-    md_ret_require=(
-        'mednafen_psx_hw_libretro.so'
-    )
+    make "${params[@]}"
+    md_ret_require="$md_build/$target"
 }
 
 function install_lr-beetle-psx() {
-    md_ret_files=(
-        'mednafen_psx_hw_libretro.so'
-    )
+    local target="mednafen_psx_libretro.so"
+    isPlatform "x86" && target="mednafen_psx_hw_libretro.so"
+    md_ret_files="$target"
 }
 
 function configure_lr-beetle-psx() {
+    local target="mednafen_psx_libretro.so"
+    isPlatform "x86" && target="mednafen_psx_hw_libretro.so"
+
     mkRomDir "psx"
     ensureSystemretroconfig "psx"
 
-    addEmulator 0 "$md_id" "psx" "$md_inst/mednafen_psx_hw_libretro.so"
+    if ! isPlatform "x86"; then
+        # Use asyncronous CD loading on (typically low I/O) non-x86 platforms
+        setRetroArchCoreOption "beetle_psx_cd_access_method" "async"
+        # enable the dynamic recompiler for non-x86 platforms
+        setRetroArchCoreOption "beetle_psx_cpu_dynarec" "execute"
+    fi
+
+    addEmulator 0 "$md_id" "psx" "$md_inst/$target"
     addSystem "psx"
 }
